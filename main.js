d25d0a8a3c7c0647e51d4a8cf92e6f6b0a5110a6
David Fernandez <dfernandez06@gmail.com>
2020-02-03 23:35:50 -0500

    Let's be super finnicky about stuff nobody else will notice

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-let menuChecks = document.getElementsByClassName("menuCheck");
-for(let checkbox of menuChecks) {
-  checkbox.addEventListener('click', () => {
-  for(let box of menuChecks) {
-    if(box !== event.target) {
+const addMenuToggle = () => {
+  let menuChecks = document.getElementsByClassName("menuCheck");
+  let body = document.getElementsByClassName("boxBody")[0];
+  for(let checkbox of menuChecks) {
+    checkbox.addEventListener('click', () => {
+      for(let box of menuChecks) {
+        if(box !== event.target) {
+          box.checked = false;
+        }
+      }
+    });
+  }
+
+  body.addEventListener('click', () => {
+    for(let box of menuChecks) {
       box.checked = false;
     }
-  }
-});
+  })
 }
+
+addMenuToggle();
