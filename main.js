44287775f0e4517556643a5db83ede952b1217b9
David Fernandez <dfernandez06@gmail.com>
2020-02-04 00:40:36 -0500

    oh you bet we have a screensaver

--- main.js
+++ main.js
@@ -18,4 +18,36 @@ const addMenuToggle = () => {
   })
 }
 
+const enableScreensaver = () => {
+  let body = document.body;
+  let screensaver = document.getElementById('screensaver');
+
+  //using this syntax specifically because of the debounce
+  //because js context makes my nose bleed
+  body.addEventListener('mousemove', debounce(function() {
+    setScreensaverVis(true);
+  }, 30000));
+
+  screensaver.addEventListener('mousemove', function() {
+    setScreensaverVis(false);
+  });
+}
+
+const setScreensaverVis = (show) => {
+  let screensaver = document.getElementById('screensaver');
+  let list = screensaver.classList;
+  show ? list.add("visible") : list.remove("visible");
+}
+
+const debounce = (func, delay) => { 
+  let debounceTimer 
+  return function() { 
+      const context = this
+      const args = arguments 
+      clearTimeout(debounceTimer) 
+      debounceTimer = setTimeout(() => func.apply(context, args), delay) 
+  } 
+}
+
 addMenuToggle();
+enableScreensaver();
